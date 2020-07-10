aaf93a23386c4efed2dcab8d5855333e54a8d959
sun <test@email>
2020-07-10 16:56:59 +0800

    update002

--- src/main/java/com/sun/test.java
+++ src/main/java/com/sun/test.java
@@ -7,6 +7,7 @@ package com.sun;
 public class test {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
+            i=i+1;
             System.out.println(i);
         }
     }
