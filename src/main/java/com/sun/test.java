2b4ba04a5075908f57d4eba24bec9cd7f1624bfd
sun <test@email>
2020-07-10 16:18:42 +0800

    Initial commit

---
+++ src/main/java/com/sun/test.java
@@ -0,0 +1,14 @@
+package com.sun;
+
+/**
+ * @author ：MrSun
+ * @date ：Created in 2020/7/10 16:12
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("asdfafagd");
+        System.out.println("asdfafagd");
+        System.out.println("asdfafagd");
+        System.out.println("asdfafagd");
+    }
+}
